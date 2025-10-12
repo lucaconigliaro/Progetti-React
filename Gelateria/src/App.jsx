93264cbe5f16469794b994e6980060f10acd48e7
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-10-12 16:15:46 +0200

    gelateria

--- Gelateria/src/App.jsx
+++ Gelateria/src/App.jsx
@@ -1,11 +1,12 @@
+import Menu from "./components/Menu";
 
 function App() {
-
   return (
-    <>
-     
-    </>
-  )
+    <section className="section-center">
+      <h3>Nice Cream</h3>
+      <Menu />
+    </section>
+  );
 }
 
-export default App
+export default App;
